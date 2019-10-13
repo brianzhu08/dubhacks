f37a5ef5322b7a6f0feeb240b3241db8decc91b7
Brian <brianzhu@uw.edu>
2019-10-12 17:15:57 -0700

    now is runnable

---
+++ proj/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, StyleSheet } from 'react-native';
+
+const Login = () => {
+    return <Text style={styles.text}> TEST</Text>;
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 30
+    }
+});
+
+export default Login;
